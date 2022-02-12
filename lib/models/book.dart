f011cf051e5c4f4f778c97e93b9e6c46cdf55dec
arteevraina <arteevraina@gmail.com>
2022-02-12 19:40:20 +0530

    refactor: removed commented out code

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -4,33 +4,6 @@ import 'package:hive/hive.dart';
 part 'book.g.dart';
 part 'book.freezed.dart';
 
-// @HiveType(typeId: 0)
-// class Book extends HiveObject {
-//   // Unique id of the book depends on DateTime.
-//   @HiveField(0)
-//   final String id;
-//   // Name of the book.
-//   @HiveField(1)
-//   final String name;
-//   // These will be used to calculate the percentage of book read.
-//   // Pages read in the book.
-//   @HiveField(2)
-//   final double pagesRead;
-//   // Pages in the book.
-//   @HiveField(3)
-//   final double totalPages;
-//   // Whether this book is favourite or not.
-//   @HiveField(4)
-//   final bool favorite;
-
-//   Book(
-//     this.id,
-//     this.name,
-//     this.pagesRead,
-//     this.totalPages,
-//     this.favorite,
-//   );
-// }
 @freezed
 class Book with _$Book {
   const Book._();
